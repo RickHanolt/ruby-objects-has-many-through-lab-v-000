80aa899a6627f0be3aeab1a662bd13b0f8a5a950
Richard Hanolt <rhanolt@gmail.com>
2020-05-25 03:57:41 +0000

    Automatically backed up by Learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -19,11 +19,11 @@ class Doctor
   end
 
   def appointments
-    Appointment.all.select{|appointment| appointment.doctor = self}
+    Appointment.all.select{|appointment| appointment.doctor == self}
   end
 
   def patients
     appointments.collect{|appointment| appointment.patient}
   end
-  
+
 end
