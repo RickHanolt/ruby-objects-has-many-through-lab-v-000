443b25f591b8b19ba65118e29c063e217c62d62f
Richard Hanolt <rhanolt@gmail.com>
2020-05-25 03:55:07 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -17,5 +17,9 @@ class Patient
   def new_appointment(date, doctor)
     Appointment.new(date, self, doctor)
   end
-  
+
+  def appointments
+    Appointment.all.select{|appointment| appointment.patient = self}
+  end
+
 end
